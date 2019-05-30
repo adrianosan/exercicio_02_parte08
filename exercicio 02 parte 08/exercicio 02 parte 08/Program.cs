034667830e91667c1a36559f05bd637ee87fe689
adriano.santos <adriano.santos@squadra.com.br>
2019-05-30 17:31:24 -0300

    exercicio 02 parte 08

---
+++ exercicio 02 parte 08/exercicio 02 parte 08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_02_parte_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Digite um numero.");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um numero.");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um numero.");
+            c = int.Parse(Console.ReadLine());
+
+            if (a > (b + c))
+            {
+                Console.WriteLine(a +" é maior que"+ b +" + "+ c);
+            }
+           
+        }
+    }
+}
